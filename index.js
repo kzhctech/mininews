1236a2c284f7875db0d157c485b51c8ec1a595c1
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-10-06 12:06:21 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,424 @@
+const startingLocation = window.location.pathname.substring(1);
+//alert(startingLocation);
+
+var cmnty;
+var cmnt;
+var dtail;
+var bat1img,bat2img,bowlerimg;
+
+function scroll(){
+  let elm = document.getElementById("myDIV");
+  setTimeout(function() {
+    elm.scrollTop = elm.scrollHeight;
+  }, 300);
+  document.getElementById('usrnme').value = localStorage.getItem("name");
+}
+
+/* Midwicket */
+
+
+function midwiket(run){
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f7");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "midwiket-ball 4s";
+  }
+  else{
+    ball.style.animation = "midwiket-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "midwiket-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  
+}
+
+
+/* Extracover */
+
+function deepExtracover(run){
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f8");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "deepExtracover-ball 4s";
+  }
+  else{
+    ball.style.animation = "deepExtracover-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "deepExtracover-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  
+}
+
+
+/* Thirdman */
+
+  function thirdman(run){
+  
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f2");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "thirdman-ball 4s";
+  }
+  else{
+    ball.style.animation = "thirdman-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "thirdman-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+    
+    
+  }
+
+/* squreLeg */
+
+  function squreLeg(run){
+  
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f1");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "squreLeg-ball 4s";
+  }
+  else{
+    ball.style.animation = "squreLeg-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "squreLeg-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  }
+
+
+
+/*  point */
+
+  function point(run){
+  
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f9");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "point-ball 4s";
+  }
+  else{
+    ball.style.animation = "point-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "point-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  }
+
+/*  midOff */
+
+  function midOff(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f3");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "midOff-ball 4s";
+  }
+  else{
+    ball.style.animation = "midOff-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "midOff-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+/*  midOn */
+
+  function midOn(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f4");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "midOn-ball 4s";
+  }
+  else{
+    ball.style.animation = "midOn-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "midOn-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+
+/* fineLeg */
+
+  function fineLeg(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f5");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "fineLeg-ball 4s";
+  }
+  else{
+    ball.style.animation = "fineLeg-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "fineLeg-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+/* gully */
+
+  function gully(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f6");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "gully-ball 4s";
+  }
+  else{
+    ball.style.animation = "gully-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "gully-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+  function showAD(){
+    document.getElementById("adv").style.display = "block";
+  }
+
+  
+  function hideAD(){
+    document.getElementById("adv").style.display = "none";
+  }
+
+
+  function updateScore(sco){
+
+    if(sco.BatName1){
+    document.getElementById("Teambat1name").innerHTML = sco.BatName1;  
+    document.getElementById("bat1Score").innerHTML = sco.BatRun1;
+     let ovr = sco.BatRun1.split(' ');
+    ovr = ovr[ovr.length - 1];
+    console.log(ovr);
+    if (ovr.includes(".")) {
+      hideAD();
+    }
+    else{
+      showAD();
+    }
+    }
+ 
+    
+    if(sco.BatName2){
+      document.getElementById("Teambat2name").innerHTML = sco.BatName2;  
+      document.getElementById("bat2Score").innerHTML = sco.BatRun2; 
+      
+    }
+
+
+    
+ }
+ 
+ 
+
+function updateit(sts){
+   document.getElementById("statusbar").innerHTML = sts;  
+}
+function updatePS(sts){
+   document.getElementById("ps").innerHTML = sts;  
+}
+function updateLW(sts){
+   document.getElementById("lw").innerHTML = sts;  
+}
+
+// function updateTitle(tit){
+//    document.getElementById("vs").innerHTML = tit;  
+// }
+
+
+function updateBat1(name,run){
+   document.getElementById("bat1name").innerHTML = name;  
+   document.getElementById("bat1run").innerHTML = run;  
+}
+
+function updateBat2(name,run){
+  document.getElementById("bat2name").innerHTML = name;  
+  document.getElementById("bat2run").innerHTML = run;  
+}
+
+
+function updateBowler(name,over,wiket){
+   document.getElementById("bowlerName").innerHTML = name;
+   document.getElementById("bowlerOW").innerHTML = over + "-" + wiket;  
+}
+
+function updatelbb(lbb,lb){
+   document.getElementById("lbb").innerHTML = lbb;
+   document.getElementById("lb").innerHTML = lb;
+}
+
+
+
+
+setInterval(function() {
+  
+  //alert(ballPossition);
+  if (ballPossition.includes("point")) {
+     point(ballEvent);
+   }
+   
+   else if (ballPossition.includes("mid-on")) {
+     midOn(ballEvent);
+   }
+ 
+   else if (ballPossition.includes("mid-of")) {
+     midOff(ballEvent);
+   }
+ 
+   else if (ballPossition.includes("mid-wicket")) {
+     midwiket(ballEvent);
+   }
+   
+   else if (ballPossition.includes("square")) {
+     squreLeg(ballEvent);
+   }
+   
+   else if (ballPossition.includes("third")) {
+     thirdman(ballEvent);
+   }
+   
+   else if (ballPossition.includes("cover")) {
+     deepExtracover(ballEvent);
+   }
+ 
+   else if (ballPossition.includes("long-on")) {
+     midwiket(ballEvent);
+   }
+   else if (ballPossition.includes("long-of")) {
+     deepExtracover(ballEvent);
+   }
+   else if (ballPossition.includes("fine-leg")) {
+     fineLeg(ballEvent);
+   }
+   else if (ballPossition.includes("gully")) {
+     gully(ballEvent);
+   }
+   console.log(ballPossition)
+},6000)
+
+function setPosition(pst,run){
+  ballPossition = pst;
+  ballEvent = run;
+}
+
+
+
+// document.querySelector('button').onclick = () => {
+
+//   const text = document.querySelector('input').value;
+//   socket.emit('message', text)
+  
+// }
+
+
+/*
+localStorage.setItem("name", "Smith");
+localStorage.getItem("name");
+*/
+
+function nameSet(){
+  let name = document.getElementById('usrnme').value;
+  localStorage.setItem("name", name);
+}
+
+function chat(){
+  let name;
+  if(!localStorage.getItem("name")){
+    name =  prompt("Please enter your name", "");
+    if (name != null) {
+      localStorage.setItem("name", name);
+      name = localStorage.getItem("name");
+    }
+    else{
+      name = "Announms";
+    }
+  }else{
+    name = localStorage.getItem("name");
+  }
+  let body = document.querySelector('#usrmsg').value;
+  let elm = document.getElementById("myDIV");
+  setTimeout(function() {
+    elm.scrollTop = elm.scrollHeight;
+  }, 300);
+
+console.log(localStorage.getItem("name"));
+
+  elm.scrollTop = elm.scrollHeight;
+  socket.emit('message', {name,body});
+  document.querySelector('#usrmsg').value = "";
+}
+
+// setInterval( function() {
+//   let elm = document.getElementById("myDIV");
+//   elm.scrollTop = elm.scrollHeight;
+//     console.log('hi');
+// },1000);
